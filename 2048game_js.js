adffaa55ad6623168503eb1b7501d08a02942845
kiranpalakurthy <kiran.palakurthy24@gmail.com>
2015-06-20 03:30:42 -0500

    Fixed right arrow & random generation

--- 2048game_js.js
+++ 2048game_js.js
@@ -13,8 +13,6 @@ var arr = new Array(3);
 function randomnumbergenerator()
 {
  
- 
- 
  var x = Math.floor(Math.random()*15+1);
  console.log("val of x:" + x);
  var y = Math.floor(Math.random()*15+1);
@@ -49,44 +47,63 @@ function draw(){
 }
 $(document).keydown(function(e) {
 	var m;
+	
   if(e.which == 37) { // left     
       alert("down");
   }
   else if(e.which == 39) { // right     
       for(var i=3; i>=0; i--){
 		  for(var j=3; j>=0; j--){
-			  if(arr[i][j] == arr[i][j-1] && arr[i][j]!=0){
-				  arr[i][j]=2*arr[i][j];
-				  arr[i][j-1]=0;
-				  
+			  
+			  if (arr[i][j] == 0) {
+				  var k=j-1;
+				  while (k >=0 && arr[i][k] == 0) { 
+					k--;
+				  }	
+				  if (k >= 0 ) {
+					arr[i][j] = arr[i][k];
+					arr[i][k] = 0;	  	
 				  }
-				 
-			   else if((arr[i][j] == 0) && (j-1>=0)){
-				  arr[i][j] = arr[i][j-1];
-				  arr[i][j-1]=0;
-				  if(arr[i][j]==arr[i][j+1] && arr[i][j]!=0){
-					  arr[i][j+1]=2*arr[i][j];
-					  arr[i][j]=0;
+				}
+			  var next = j-1;
+			  if (arr[i][next] == 0) {
+				  var k=next-1;
+				  while (k >=0 && arr[i][k] == 0) { 
+					k--;
+				  }	
+				  if (k >= 0 ) {
+					arr[i][next] = arr[i][k];
+					arr[i][k] = 0;	  	
 				  }
-				  m=j;
-				  if(arr[i][j]!=0 && arr[i][j+1]==0 && j+1<=3){
-				  while((m<3) && (arr[i][m+1] == 0)){
-					  
-						  m++;
-					  
-					  
-					  }
-					  arr[i][m]=arr[i][j];
-					  arr[i][j]=0;
-				  }
-				  }
-				 
-				 
-		  }
+				}
+
+				if (arr[i][j] == arr[i][j-1]) {
+				  arr[i][j] = 2 * arr[i][j];
+				  arr[i][j-1] = 0;
+			  }
 		  }
 	  }
-	  
-  
+  }
+  rand();
   draw();
 });
+function rand(){
+	var ran = [];
+	var i,j;
+	for(i=0;i<=3;i++){
+	  for(j=0;j<=3;j++){
+		  if(arr[i][j]==0){
+			 ran.push({
+            i: i, 
+            j: j
+        });
+		  }
+	  }
+  }	
+   var z=ran.length;  
+   var random_number = Math.floor(Math.random()*z);
+   var k=ran[random_number].i;
+   var l=ran[random_number].j;
+   arr[k][l] = 2;   
+}
  
