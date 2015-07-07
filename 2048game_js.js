24cc18bcce8df92c37993b11f1ea2a5b82ec98c9
kiranpalakurthy <kiran.palakurthy24@gmail.com>
2015-07-06 20:26:40 -0700

    game over overlay

--- 2048game_js.js
+++ 2048game_js.js
@@ -1,10 +1,10 @@
 var arr = new Array(4);
 var scr=$("#score");
-
  for(var i=0; i<4; i++){
 	 arr[i]=new Array(4);
  }
  function newGame(){
+	$('#myModal').modal('hide');
 	for(var i=0; i<4; i++){
 	 for(var j=0; j<4; j++){
 		 arr[i][j]=0;
@@ -13,7 +13,6 @@ var scr=$("#score");
  }
  randomnumbergenerator();
 }
-
 function randomnumbergenerator()
 {
  var rannum=Math.random();
@@ -42,6 +41,7 @@ function randomnumbergenerator()
 function draw(){
 	$('#score').text(scr);
 	var s = [];
+	
 	for (var i=0; i< 4; i++) {
 		s.push('<div class="row">');
 		for (var j=0; j<4; j++) {
@@ -88,14 +88,11 @@ function draw(){
 		s.push('</div>');	
 		
 	}	
+	
 	document.getElementById("division").innerHTML= s.join('');		
-}
-$(document).keydown(function(e) {
-	var initial = arr;
-	console.log(initial);
-	var m;
-	if(e.which == 38){//up
-	 for(var j=0; j<=3; j++){
+	}
+function up(){
+	for(var j=0; j<=3; j++){
 		  for(var i=0; i<=3; i++){
 			  
 			  if (arr[i][j] == 0) {
@@ -127,9 +124,9 @@ $(document).keydown(function(e) {
 			  }
 		  }
 	  }
-	}
-	else if(e.which == 40){//down
-	 for(var j=3; j>=0; j--){
+}	
+function down(){
+	for(var j=3; j>=0; j--){
 		  for(var i=3; i>=0; i--){
 			  
 			  if (arr[i][j] == 0) {
@@ -161,9 +158,9 @@ $(document).keydown(function(e) {
 			  }
 		  }
 	  }
-	}
-  else if(e.which == 37) { // left     
-      for(var i=0; i<=3; i++){
+}
+function left(){
+	for(var i=0; i<=3; i++){
 		  for(var j=0; j<=3; j++){
 			  
 			  if (arr[i][j] == 0) {
@@ -195,9 +192,9 @@ $(document).keydown(function(e) {
 			  }
 		  }
 	  }
-  }
-  else if(e.which == 39) { // right     
-      for(var i=3; i>=0; i--){
+}
+function right(){
+	for(var i=3; i>=0; i--){
 		  for(var j=3; j>=0; j--){
 			  
 			  if (arr[i][j] == 0) {
@@ -229,16 +226,56 @@ $(document).keydown(function(e) {
 			  }
 		  }
 	  }
+}
+$(document).keydown(function(e) {
+	var initial = JSON.parse(JSON.stringify(arr));
+	console.log(initial);
+	var m;
+	if(e.which == 38){//up
+	 up();
+	}
+	else if(e.which == 40){//down
+	 down();
+	}
+  else if(e.which == 37) { // left     
+      left();
+  }
+  else if(e.which == 39) { // right     
+      right();
   }
   
   if((e.which === 37) || (e.which === 38) || (e.which === 39) || (e.which === 40) ){
-  rand();
+  arraycompare(initial, arr);
   draw();
   }
+  losingcondition();
 });
 function arraycompare(initial, actual){
+	console.log("initial:" + initial);
+	console.log("actual:" + actual);
+	var flag;
+	if(initial.toString() === 	actual.toString()) {
+		console.log("equal");
+		
+	} else {
+		console.log("Not equal");
+		rand();
+	}
 	
-	
+}
+function losingcondition(){
+	var k=0;
+	for(var i=0;i<=3;i++){
+		for(var j=0;j<=3;j++){
+			if(arr[i][j]!=0){
+				k++;
+			}
+			
+		}
+	}
+	if(k==16){
+	   losingconditioncheck();
+	}
 }
 function rand(){
 	var ran = [];
@@ -257,6 +294,160 @@ function rand(){
    var random_number = Math.floor(Math.random()*z);
    var k=ran[random_number].i;
    var l=ran[random_number].j;
-   arr[k][l] = 2;   
+   var u=Math.random();
+   if(u<=0.8){
+	arr[k][l] = 2; 
+   }   
+   else{
+	   arr[k][l]=4;
+   }
 }
- 
+function losingconditioncheck(){
+	var initial = JSON.parse(JSON.stringify(arr));
+	console.log(initial);
+	var m;
+	var ename=38;
+	if(ename == 38){//up
+	 for(var j=0; j<=3; j++){
+		  for(var i=0; i<=3; i++){
+			  
+			  if (initial[i][j] == 0) {
+				  var k=i+1;
+				  while (k <= 3 && initial[k][j] == 0) { 
+					k++;
+				  }	
+				  if (k <= 3) {
+					initial[i][j] = initial[k][j];
+				    initial[k][j] = 0;	  	
+				  }
+				}
+			  var next = i+1;
+			  if (next<3 && initial[next][j] == 0) {
+				  var k=next+1;
+				  while (k <=3 && initial[k][j] == 0) { 
+					k++;
+				  }	
+				  if (k <= 3 ) {
+					initial[next][j] = initial[k][j];
+					initial[k][j] = 0;	  	
+				  }
+				}
+
+				if (i+1 <= 3 && initial[i][j] == initial[i+1][j]) {
+				  initial[i][j] = 2 * initial[i][j];
+				  initial[i+1][j] = 0;
+				  scr=parseInt(scr)+initial[i][j];
+			  }
+		  }
+	  }
+	}
+	ename=40;
+	if(ename == 40){//down
+	 for(var j=3; j>=0; j--){
+		  for(var i=3; i>=0; i--){
+			  
+			  if (initial[i][j] == 0) {
+				  var k=i-1;
+				  while (k >= 0 && initial[k][j] == 0) { 
+					k--;
+				  }	
+				  if (k >= 0) {
+					initial[i][j] = initial[k][j];
+					initial[k][j] = 0;	  	
+				  }
+				}
+			  var next = i-1;
+			  if (next>0 && initial[next][j] == 0) {
+				  var k=next-1;
+				  while (k >= 0 && initial[k][j] == 0) { 
+					k--;
+				  }	
+				  if (k >= 0 ) {
+					initial[next][j] = initial[k][j];
+					initial[k][j] = 0;	  	
+				  }
+				}
+
+				if (i-1 >= 0 && initial[i][j] == initial[i-1][j]) {
+				  initial[i][j] = 2 * initial[i][j];
+				  initial[i-1][j] = 0;
+				  scr=parseInt(scr)+initial[i][j];
+			  }
+		  }
+	  }
+	}
+	ename=37;
+  if(ename == 37) { // left     
+      for(var i=0; i<=3; i++){
+		  for(var j=0; j<=3; j++){
+			  
+			  if (initial[i][j] == 0) {
+				  var k=j+1;
+				  while (k <= 3 && initial[i][k] == 0) { 
+					k++;
+				  }	
+				  if (k <= 3) {
+					initial[i][j] = initial[i][k];
+					initial[i][k] = 0;	  	
+				  }
+				}
+			  var next = j+1;
+			  if (initial[i][next] == 0) {
+				  var k=next+1;
+				  while (k <=3 && initial[i][k] == 0) { 
+					k++;
+				  }	
+				  if (k <= 3 ) {
+					initial[i][next] = initial[i][k];
+					initial[i][k] = 0;	  	
+				  }
+				}
+
+				if (initial[i][j] == initial[i][j+1]) {
+				  initial[i][j] = 2 * initial[i][j];
+				  initial[i][j+1] = 0;
+				  scr=parseInt(scr)+initial[i][j];
+			  }
+		  }
+	  }
+  }
+  ename=39;
+  if(ename == 39) { // right     
+      for(var i=3; i>=0; i--){
+		  for(var j=3; j>=0; j--){
+			  
+			  if (initial[i][j] == 0) {
+				  var k=j-1;
+				  while (k >=0 && initial[i][k] == 0) { 
+					k--;
+				  }	
+				  if (k >= 0 ) {
+					initial[i][j] = initial[i][k];
+					initial[i][k] = 0;	  	
+				  }
+				}
+			  var next = j-1;
+			  if (initial[i][next] == 0) {
+				  var k=next-1;
+				  while (k >=0 && initial[i][k] == 0) { 
+					k--;
+				  }	
+				  if (k >= 0 ) {
+					initial[i][next] = initial[i][k];
+					initial[i][k] = 0;	  	
+				  }
+				}
+
+				if (initial[i][j] == initial[i][j-1]) {
+				  initial[i][j] = 2 * initial[i][j];
+				  scr=parseInt(scr)+initial[i][j];
+				  initial[i][j-1] = 0;
+			  }
+		  }
+	  }
+  }
+  if(initial.toString() === arr.toString()) {
+	   $('#myModal').modal('show');
+	}
+	
+}
